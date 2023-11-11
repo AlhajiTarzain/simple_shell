1654ea74bb892ff1dd17e74489af4481e061955f
AlhajiTarzain <dredeen21@gmail.com>
2023-11-11 07:54:34 -0800

    mai

---
+++ header.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/*prototypes*/
+
+#endif
---
+++ main.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * main - program stats.
+ * Return: program status.
+ */
+
+int main(void)
+{
+	char *prompt = "THE BIG BASH:$ ";
+	char *input = NULL;
+	char **token_arr;
+	int result = 0;
+	size_t size = 0;
+	ssize_t iread;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			_myprintf(prompt);
+		iread = getline(&input, &size, stdin);
+		if (iread == -1)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				myputchar('\n');
+			break;
+		}
+		/*Remove the trailing newline character */
+		if (input[iread - 1] == '\n')
+			input[iread - 1] = '\0';
+
+		token_arr = mktoken(input, " \t");
+		if (token_arr[0] == NULL)
+		{
+			free(token_arr);
+			continue;
+		}
+		if (comp(input, "exit") == 1)
+		{
+			_myprintf("Exiting shell...\n");
+			exit(0);
+		}
+
+		result = inbuilt(token_arr, input, result);
+		if (result == 1)
+			result = checker(token_arr, input);
+	}
+	free(input);
+	return (result);
+}
+
